aa3c188d5064b50b3b6b77fad7e66b8c96351f28
BagasIbrahim <bagasibrahim9@gmail.com>
2023-05-11 15:58:19 +0700

    [FIX] merubah codingan pada file untuk tugas pertemuan 19

--- pages/users_create.php
+++ pages/users_create.php
@@ -1,34 +1,26 @@
 <?php
 require_once 'db_koneksi.php';
 
-// if (isset($_POST['submit'])) {
-//     $targetDir = "../assets/images/";
-//     $targetFile = $targetDir . basename($_FILES["avatar"]["name"]);
+// Ambil data dari form
+$name = $_POST['name'];
+$role = $_POST['role'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$telp = $_POST['telp'];
+$alamat = $_POST['alamat'];
 
-//     // Ambil data dari form
-//     if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $targetFile)) {
-//         echo "The file " . basename($_FILES["avatar"]["name"]) . " has been uploaded.";
-//     } else {
-//         echo "Sorry, there was an error uploading your file.";
-//     }
-    $name = $_POST['name'];
-    $role = $_POST['role'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $telp = $_POST['telp'];
-    $alamat = $_POST['alamat'];
 
-    // Query untuk menambah data
-    $query = "INSERT INTO users (name, role, password, email, phone, address) 
-VALUES ('$name', '$role', '$password', '$email', '$telp', '$alamat',)";
+// Query untuk menambah data
+$query = "INSERT INTO users (name, role, password, email, phone, address) 
+VALUES ('$name', '$role', '$password', '$email', '$telp', '$alamat')";
 
-    // Eksekusi query
-    if (mysqli_query($conn, $query)) {
-        header('Location: users_read.php');
-        echo 'Data berhasil ditambahkan';
-    } else {
-        echo 'Error: ' . mysqli_error($conn);
-    }
+// Eksekusi query
+if (mysqli_query($conn, $query)) {
+    header('Location: users_read.php');
+    echo 'Data berhasil ditambahkan';
+} else {
+    echo 'Error: ' . mysqli_error($conn);
+}
 
-    // Tutup koneksi
-    mysqli_close($conn);
+// Tutup koneksi
+mysqli_close($conn);
